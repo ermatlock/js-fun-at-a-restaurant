f6a727e522beb9b1bc9b065068b5dbfbb02535fa
Eric Matlock <ermatlock@gmail.com>
2021-12-03 08:19:41 -0500

    Complete meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
+};
 
-}
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
+
+function addIngredients(item, ingredientList) {
+  for (var i = 0; i < ingredientList.length; i ++) {
+    if (item === ingredientList[i]) {
+    return;
+    };
+  };
+  ingredientList.push(item);
+  return ingredientList;
+};
+
+function formatPrice(price) {
+  return `$${price}`;
+};
+
+function decreasePrice(price) {
+  return price - (price * .1);
+};
+
+function createRecipe(title, ingredientList, menuType) {
+  recipe = {
+    title: title,
+    ingredients: ingredientList,
+    type: menuType,
+  };
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
+};
