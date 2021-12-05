6941ead209e35da30759d1a98a4b8661ba0c0797
Eric Matlock <ermatlock@gmail.com>
2021-12-05 09:05:33 -0500

    Complete chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,26 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+
+  greetCustomer(customerName, isMorning) {
+    if (!isMorning) {
+      return `Hello, ${customerName}!`;
+    } else {
+      return `Good morning, ${customerName}!`
+    }
+  }
+
+  checkForFood(foodItem) {
+    for (var i = 0; i < this.restaurant.menus.breakfast.length; i++) {
+      if (foodItem === this.restaurant.menus.breakfast[i]) {
+        return `Yes, we're serving ${foodItem.name} today!`
+      }
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`
+  }
+
+};
 
 module.exports = Chef;
