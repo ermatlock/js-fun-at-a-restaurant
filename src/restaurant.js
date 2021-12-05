97a95df6526c4763d5ed60d30801ce99439bb0b2
Eric Matlock <ermatlock@gmail.com>
2021-12-05 00:16:23 -0500

    Complete restaurant begin chef

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,57 @@
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return restaurant;
+};
+
+function addMenuItem(restaurant, menuItem) {
+  if (menuItem.type === 'breakfast')
+  // if (menuItem.type === 'breakfast' && !restaurant.menus.breakfast.includes(menuItem)) {
+  //   restaurant.menus.breakfast.push(menuItem);
+  // } else if (menuItem.type === 'lunch' && !restaurant.menus.lunch.includes(menuItem)) {
+  //     restaurant.menus.lunch.push(menuItem);
+  // } else if (menuItem.type === 'dinner' && !restaurant.menus.dinner.includes(menuItem)) {
+  //     restaurant.menus.dinner.push(menuItem);
+  // }
+};
+
+function removeMenuItem(restaurant, menuItemName, type) {
+  if (type === 'breakfast') {
+    for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
+      if (restaurant.menus.breakfast[i].name === menuItemName) {
+        restaurant.menus.breakfast.splice(i, 1);
+        return `No one is eating our ${menuItemName} - it has been removed from the ${type} menu!`
+      }
+    }
+    return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`
+    } else if (type === 'lunch') {
+      for (var i = 0; i < restaurant.menus.lunch.length; i++) {
+        if (restaurant.menus.lunch[i].name === menuItemName) {
+          restaurant.menus.lunch.splice(i, 1);
+          return `No one is eating our ${menuItemName} - it has been removed from the ${type} menu!`
+      }
+    }
+      return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`
+    } else if (type === 'dinner') {
+      for (var i = 0; i < restaurant.menus.dinner.length; i++) {
+        if (restaurant.menus.dinner[i].name === menuItemName) {
+          restaurant.menus.dinner.splice(i, 1);
+          return `No one is eating our ${menuItemName} - it has been removed from the ${type} menu!`
+        }
+    }
+      return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`
+    }
+};
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
